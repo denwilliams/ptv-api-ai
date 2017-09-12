e22362dc5adc6ebb5751859484d9789603ad6650
Den Williams <work@denwilliams.net>
2017-09-12 22:35:17 +1000

    test response

--- handler.js
+++ handler.js
@@ -4,8 +4,7 @@ module.exports.apiaiHook = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      speech: 'Go Serverless v1.0! Your function executed successfully!'
     }),
   };
 
