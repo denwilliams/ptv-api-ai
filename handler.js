720dc3da0684d5a8e28431f770eca736a8213e26
Den Williams <work@denwilliams.net>
2017-09-14 21:53:19 +1000

    wip

--- handler.js
+++ handler.js
@@ -1,15 +1,32 @@
 'use strict';
 
-module.exports.apiaiHook = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      speech: 'Go Serverless v1.0! Your function executed successfully!'
-    }),
-  };
+const got = require('got');
 
-  callback(null, response);
+module.exports.apiaiHook = (event, context, callback) => {
+  got(process.env.PTV_URL, { json: true })
+  .then(r => r.body)
+  .then(body => {
+    const next = body.departures[0];
+    const date = new Date(next.estimated_departure_utc || next.scheduled_departure_utc);
+    const diff = (date - new Date()) / 60000;
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        speech: `The next service departs in ${formatMins(diff)} minutes`
+      }),
+    };
+
+    callback(null, response);
+  })
+  .catch(err => {
+    callback(err);
+  });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+function formatMins(mins) {
+  if (mins > 10) return mins.toFixed(0);
+  return mins.toFixed(1);
+}
